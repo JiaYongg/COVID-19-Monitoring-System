e22aa8a8b58ae8aa2d54fd558f3b6764fb1dafca
Shu Yih Jordan Choi <s10208161@connect.np.edu.sg>
2021-01-20 19:27:51 +0800

    SafeEntry Class Implemented

---
+++ PRG2_Assignment_Team5/SafeEntry.cs
@@ -0,0 +1,43 @@
+﻿//============================================================
+// Student Number : S10208161, S10202579
+// Student Name : Jordan Choi, Poh Jia Yong
+// Module Group : T01
+//============================================================
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRG2_Assignment_Team5
+{
+    class SafeEntry
+    {
+        // properties
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public BusinessLocation Location { get; set; }
+
+        // default constructor
+        public SafeEntry() { }
+        
+        // parameterized constructors
+        public SafeEntry(DateTime ci, BusinessLocation loc)
+        {
+            CheckIn = ci;
+            Location = loc;
+        }
+
+        // Methods
+        public void PerformCheckOut()
+        {
+            CheckOut = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return "Check In: " + CheckIn + "\tCheck Out:" + CheckOut + "\tLocation: " + Location;
+        }
+
+
+    }
+}
