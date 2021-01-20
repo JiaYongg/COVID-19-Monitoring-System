f0486f738b04cf3f82f8783534cc0a91c6659d4f
Shu Yih Jordan Choi <s10208161@connect.np.edu.sg>
2021-01-20 18:28:56 +0800

    TravelEntry Class Partially Implemented

---
+++ PRG2_Assignment_Team5/TravelEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRG2_Assignment_Team5
+{
+    class TravelEntry
+    {
+        // Properties
+        public string LastCountryOfEmbarkation { get; set; }
+        public string EntryMode { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime ShnEndDate { get; set; }
+        public SHNFacility ShnStay { get; set; }
+        public bool IsPaid { get; set; }
+    
+        // Default Constructor
+        public TravelEntry() { }
+
+        // Parameterized Constructor
+        public TravelEntry(string lcoe, string em, DateTime ed)
+        {
+            LastCountryOfEmbarkation = lcoe;
+            EntryMode = em;
+            EntryDate = ed;
+        }
+
+        // Methods
+        public void AssignSHNFacility(SHNFacility ShnFac)
+        {
+            ShnStay = ShnFac;
+        }
+        // to add
+        // 1) AssignSHNFacility(SHNFacility)
+        // 2) CalculateSHNDuration()
+        // 3) ToString():string
+    }
+}
